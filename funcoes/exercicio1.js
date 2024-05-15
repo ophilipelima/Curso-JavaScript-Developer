ffe1c18fe851853c7e55c8a0fa097b6f72a66ddd
Philipe Henrique de Sousa Lima <philipehenri.lima@gmail.com>
2024-05-15 14:11:20 -0300

    addFunctionVerificarIdade

--- funcoes/exercicio1.js
+++ funcoes/exercicio1.js
@@ -1,6 +1,15 @@
 function escrevaMeuNome(nome){
     console.log('Meu nome é ' + nome);
 }
+
+function verificarIdade(idade){
+    if(idade >=18){
+        console.log('Maior');
+    }else{
+        console.log('Menor');
+    }
+}
 (function main(){
     escrevaMeuNome('Philipe');
+    verificarIdade(20);
 })();
