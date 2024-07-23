3eecfcfe143d989fb4c9cd3fd719714ddb38745d
Philipe Henrique de Sousa Lima <philipehenri.lima@gmail.com>
2024-07-23 15:45:26 -0300

    exercicioForConcluido

---
+++ arraysElacoFor/exercicio.js
@@ -0,0 +1,8 @@
+// Crie um programa que dado um número imprima a sua tabuada]
+
+const numero = 6;
+
+for(contador = 0; contador <= 10; contador++){
+    tabuada = contador * numero;
+    console.log(numero, ' * ', contador, ' = ', tabuada);
+}
